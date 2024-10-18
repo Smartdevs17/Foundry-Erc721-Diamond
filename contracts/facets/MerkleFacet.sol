0d6900748df72dbd83e74aa4149309d2846d7912
smartdev <smartdevs17@gmail.com>
2024-10-18 17:39:52 +0100

    added merkle tree stuffs

--- contracts/facets/MerkleFacet.sol
+++ contracts/facets/MerkleFacet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "../libraries/LibDiamond.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleFacet {
--- contracts/facets/PresaleFacet.sol
+++ contracts/facets/PresaleFacet.sol
@@ -4,13 +4,29 @@ pragma solidity ^0.8.0;
 import "../libraries/LibDiamond.sol";
 
 contract PresaleFacet {
-    uint256 public constant PRICE_PER_NFT = 1 ether / 30;
+    uint256 public constant PRICE_PER_NFT = 33333333333333333; // 1 ether / 30 as an integer
     uint256 public constant MIN_PURCHASE = 0.01 ether;
+
+    // Declare the nftFacet variable
+    INFTFacet public nftFacet;
+
+    // Add a constructor to initialize nftFacet
+    constructor(address _nftFacetAddress) {
+        nftFacet = INFTFacet(_nftFacetAddress);
+    }
 
     function buyNFTs() external payable {
         require(msg.value >= MIN_PURCHASE, "Minimum purchase is 0.01 ETH");
 
         uint256 numberOfNFTs = msg.value / PRICE_PER_NFT;
         // Logic to mint and transfer NFTs to msg.sender
+        nftFacet.mint(msg.sender, numberOfNFTs);
+        payable(msg.sender).transfer(msg.value - (numberOfNFTs * PRICE_PER_NFT));
+        // emit NFTMinted(msg.sender, numberOfNFTs);
     }
+}
+
+// Define the interface for the NFTFacet contract
+interface INFTFacet {
+    function mint(address to, uint256 numberOfNFTs) external;
 }
